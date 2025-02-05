b213d069bcf4edb3f065b78d9d3910153431c36c
unknown <ydev2897@gmail.com>
2025-02-05 21:23:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://study-app1.vercel.app",
+    origin: "https://front-end-jade-nu.vercel.app",
     credentials: true,
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     allowedHeaders: [
